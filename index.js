2050b92f8286786c601e9290d4d4519adcbc68fe
Kosicherry <106801608+Kosicherry@users.noreply.github.com>
2022-08-29 17:41:20 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Root } from './Root';
+import { loadStdlib } from '@reach-sh/stdlib';
+import { ALGO_MyAlgoConnect as MyAlgoConnect } from '@reach-sh/stdlib';
+const reach = loadStdlib("ALGO");
+reach.setWalletFallback(reach.walletFallback({
+  providerEnv: 'TestNet', MyAlgoConnect
+}));
+
+function renderDOM() {
+  ReactDOM.render(
+    <React.StrictMode><App /></React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { mode: 'ConnectAccount' }
+  }
+  async componentDidMount() {
+    const acc = await reach.getDefaultAccount();
+    const balAtomic = await reach.balanceOf(acc);
+    const bal = reach.formatCurrency(balAtomic, 4);
+    this.setState({ mode: 'Start', acc, bal });
+
+  }
+  render() {
+    const { mode } = this.state;
+    const parent = this;
+    return <Root mode={mode} parent={parent} acc={this.state.acc} />;
+  }
+}
+
+renderDOM();
